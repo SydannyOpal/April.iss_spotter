a9386791610db50351bcf7698bc312fa1de69101
= <sydanny15@gmail.com>
2022-04-27 11:11:11 -0400

    iss_spotter iii

--- Index.js
+++ Index.js
@@ -1,19 +1,16 @@
 const request = require("request");
 const { calculateMac } = require("request/lib/hawk");
 
-const  myFun  = require('./iss');
-var ipAdd;
+const myFun = require("./iss");
+var ipAdd, coords;
 const myCustomConsolelog = (error, ip) => {
-  
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
 
   ipAdd = ip;
-  console.log('It worked! Returned IP:' , ip);
+  console.log("It worked! Returned IP:", ip);
   myFun.fetchCoordsByIP(ipAdd);
-
 };
 myFun.fetchMyIP(myCustomConsolelog);
-
--- iss.js
+++ iss.js
@@ -1,40 +1,65 @@
-const request = require('request');
+const request = require("request");
 
-const fetchMyIP = function(callback) {
-  request('https://api.ipify.org/?format=json', function(error, response, body) {
+const fetchMyIP = function (callback) {
+  request(
+    "https://api.ipify.org/?format=json",
+    function (error, response, body) {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      // if non-200 status, assume server error
+      if (response.statusCode !== 200) {
+        const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+        callback(Error(msg), null);
+        return;
+      }
+      callback(null, JSON.parse(body).ip);
+    }
+  );
+};
+
+const fetchCoordsByIP = function (ip) {
+  let url =
+    "https://api.freegeoip.app/json/" +
+    ip +
+    "?apikey=3ab075a0-bcac-11ec-9038-53f35d367a56";
+  console.log(url);
+
+  request.get(url, function (error, response, body) {
     if (error) {
-      callback(error, null);
-      return;
+      // console.log("It didn't work!", error);
+      return "error:" + error;
     }
+
+    // console.log("Status return is:" + response.statusCode);
     // if non-200 status, assume server error
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
+    if (response.statusCode == 200) {
+      // const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      console.log(JSON.parse(body));
+      let obj = JSON.parse(body);
+      fetchISSFlyOverTimes(obj["latitude"], obj["longitude"]);
     }
-    callback(null,  JSON.parse(body).ip);
   });
 };
 
-const fetchCoordsByIP = function(ip) {
-  let url = 'https://api.freegeoip.app/json/'+ ip + '?apikey=3ab075a0-bcac-11ec-9038-53f35d367a56'
+const fetchISSFlyOverTimes = function (lat, lng) {
+  let url = "https://iss-pass.herokuapp.com/json/?lat=" + lat + "&lon=" + lng;
   console.log(url);
 
-request.get(url, function(error, response, body) {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  } 
+  request.get(url, function (error, response, body) {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
 
-  // console.log("Status return is:" + response.statusCode);
-  // if non-200 status, assume server error
-  if (response.statusCode == 200) {
-    // const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    console.log(JSON.parse(body));
-
-  }
-});
-
+    // console.log("Status return is:" + response.statusCode);
+    // if non-200 status, assume server error
+    if (response.statusCode == 200) {
+      // const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      console.log(JSON.parse(body));
+    }
+  });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
